b9964ccb9142ed4894b12df7dafed290183a6391
zefir <stickulzon@gmail.com>
2025-11-04 10:22:05 +0100

    Partially change how the ItemStack networking are handled

--- src/main/java/com/zefir/servercosmetics/mixin/ItemStackMixin.java
+++ src/main/java/com/zefir/servercosmetics/mixin/ItemStackMixin.java
@@ -1,9 +1,19 @@
 package com.zefir.servercosmetics.mixin;
 
+import com.mojang.serialization.Codec;
 import com.zefir.servercosmetics.ext.IItemStack;
+import com.zefir.servercosmetics.util.Utils;
+import eu.pb4.polymer.common.api.PolymerCommonUtils;
+import eu.pb4.polymer.core.api.item.PolymerItemUtils;
+import eu.pb4.polymer.core.impl.PolymerImplUtils;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import xyz.nucleoid.packettweaker.PacketContext;
+
+import java.util.function.Supplier;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin implements IItemStack {
